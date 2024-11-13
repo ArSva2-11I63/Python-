b6a9d16466bb20705160d5bfc6ba04beade91464
ArSva2-11I63 <aarnaytc1@gmail.com>
2024-11-13 22:16:23 +0530

    Create y.py

---
+++ y.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))
+
+sum = 0
+
+x = num
+while x > 0:
+   digit = x % 10
+   sum += digit ** 3
+   x //= 10
+
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
